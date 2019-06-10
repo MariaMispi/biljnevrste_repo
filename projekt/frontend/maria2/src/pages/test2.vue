38bad8a46839b62f49142c9a8ede83003467375a
MariaMispi <48434065+MariaMispi@users.noreply.github.com>
2019-06-10 23:24:20 +0200

    Prebacivanje sa forka na branch

---
+++ projekt/frontend/maria2/src/pages/test2.vue
@@ -0,0 +1,28 @@
+<template>
+    <q-page>
+     <q-list bordered>
+         <q-item>
+          {{post.data.naziv_roda}}
+         </q-item>
+     </q-list>
+    </q-page>
+</template>
+
+<script>
+export default {
+  loadData () {
+    this.$axios.get('http://193.198.97.14:8000/api/rodovi/1/')
+      .then((response) => {
+        alert(response.data.naziv_roda)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  },
+  data () {
+    return {
+      posts: []
+    }
+  }
+}
+</script>
